41a3fb8d9678419127ec54007ac539439076e744
Sergei1603 <serega_kuznetsov200@mail.ru>
2024-03-16 23:37:36 +0500

    Добавьте файлы проекта.

---
+++ rosneft/Model/Model.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Office.Interop;
+using OfficeOpenXml;
+
+
+namespace rosneft.Model
+{
+    internal class Model
+    {
+        private List<int> years;
+        public List<int> Years
+        {
+            get { return years; }
+        }
+
+
+        private List<int> flow;
+        public List<int> Flow
+        {
+            get { return flow; }
+        }
+
+        public Model()
+        {
+            Read();
+        }
+
+        void Read()
+        {
+            ExcelPackage.LicenseContext = LicenseContext.Commercial;
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+            string file = "C:\\Users\\Lenovo\\source\\repos\\rosneft\\rosneft\\Model\\Data.xlsx";
+            List<int> year = new List<int>();
+            List<int> flow = new List<int>();
+
+            List<List<string>> data = new List<List<string>>();
+
+            using (ExcelPackage package = new ExcelPackage(new FileInfo(file)))
+            {
+
+                ExcelWorksheet worksheet = package.Workbook.Worksheets.ElementAt(1);
+                if (worksheet != null)
+                {
+                    for (int i = 2; i <= worksheet.Dimension.End.Row; i++)
+                    {
+                        List<string> row = new List<string>();
+                        worksheet.Row(i);
+                        for (int j = 1; j <= worksheet.Dimension.End.Column; j++)
+                        {
+                            row.Add(worksheet.Cells[i, j].Value.ToString());
+                        }
+                        year.Add(int.Parse(row[0]));
+                        flow.Add(int.Parse(row[1]) - int.Parse(row[2]));
+                    }
+                }
+            }
+            this.years = year;
+            this.flow = flow;
+        }
+
+
+        public double calculate(double discont, int last_year)
+        {
+            double answer = 0;
+            int len = last_year - years[0] + 1;
+            int j = 1;
+            for (int i = 0; i < len; i++)
+            {
+                double tmp = flow[i] * 1 / Math.Pow(1 + discont, j);
+                answer += tmp;
+                j++;
+            }
+            return answer;
+        }
+    }
+}
---
+++ rosneft/ModelView/VMMain.cs
@@ -0,0 +1,148 @@
+﻿using Microsoft.Office.Interop.Excel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace rosneft.ModelView
+{
+    public class RelayCommand : ICommand
+    {
+        private Action<object> execute;
+        private Func<object, bool> canExecute;
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return this.canExecute == null || this.canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            this.execute(parameter);
+        }
+    }
+    internal class VMMain : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName]string prop="")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+
+
+        private double _discont = 0.2;
+        public object Discont
+        {
+            get { return _discont; }
+            set
+            {
+                double tmp;
+                if(double.TryParse((string)value, NumberStyles.Any,
+          CultureInfo.InvariantCulture, out tmp) && tmp >= 0){
+                    Eror = Visibility.Hidden;
+                    _discont = tmp;
+                }
+                else
+                {
+                    _discont = 0;
+                    Eror = Visibility.Visible;
+                    Res = 0;
+                }
+                OnPropertyChanged("Discont");
+            }
+        }
+
+
+        private Visibility _eror = Visibility.Hidden;
+        public Visibility Eror
+        {
+            get { return _eror; }
+            set
+            {
+                _eror = value;
+                OnPropertyChanged("Eror");
+            }
+        }
+
+
+        private double _res = 0;
+        public double Res
+        {
+            get { return _res; }
+            set
+            {
+                if (Eror == Visibility.Hidden)
+                {
+                    _res = value;
+                }
+                else
+                {
+                    _res = 0;
+                }
+                OnPropertyChanged("Res");
+            }
+        }
+
+
+        private List<int> _years;
+        public List<int> Years
+        {
+            get { return _years; }
+        }
+
+
+        private int _last_year = 2050;
+        public int Last_year
+        {
+            get { return _last_year; }
+            set
+            {
+                _last_year = value;
+                OnPropertyChanged("Last_year");
+            }
+        }
+
+
+        private RelayCommand addCommand;
+        public RelayCommand AddCommand
+        {
+            get
+            {
+                return addCommand ??
+                  (addCommand = new RelayCommand(obj =>
+                  {
+                      Res = r.calculate((double)Discont, _last_year);
+                      Res = Math.Round(_res, 2);
+                      OnPropertyChanged("Res");
+                  }));
+            }
+        }
+
+
+        private Model.Model r;
+
+        public VMMain()
+        {
+            r = new Model.Model();
+            _years = r.Years;
+        }
+    }
+}
